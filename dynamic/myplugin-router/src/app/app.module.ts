b0c20f55b3048ec78e151529fff80e0f481b2ca1
ln <ln@lndeMacBook-Pro.local>
2018-05-21 19:41:46 +0800

    add window scroll

--- dynamic/myplugin-router/src/app/app.module.ts
+++ dynamic/myplugin-router/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material'
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule } from '@angular/material'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,7 +12,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
